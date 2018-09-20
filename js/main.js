d555b40955a5255ebc64190e274f9b5b0864d5a6
Islam <islam.sayed8@gmail.com>
2018-09-21 01:03:50 +0200

    Add click event to copy code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 window.onload = () => {
-    
-//add class active to clicked nav-link
-const navList = document.getElementById('nav-items')
-const navListItems = navList.children
-navList.addEventListener('click', (e) => {
+
+  //add class active to clicked nav-link
+  const navList = document.getElementById('nav-items')
+  const navListItems = navList.children
+  navList.addEventListener('click', (e) => {
     for (const navListItem of navListItems) {
-        console.log(navListItem)
+      console.log(navListItem)
       navListItem.children[0].classList.remove('active')
     }
-  const eventTarget = e.target
-  console.log(navListItems)
-  
-  eventTarget.classList.add('active')
-})
+    const eventTarget = e.target
+    console.log(navListItems)
 
+    eventTarget.classList.add('active')
+  })
 
+  //code to be copied
+  const copyCodeBtn = document.getElementById('copy-code')
+  copyCodeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const copiedText = document.getElementById('code');
+    copiedText.select();
+    document.execCommand('copy');
+  })
 }
+
